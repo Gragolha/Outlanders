219f3a121264b6e5bdd07e039dc70914307dee63
Gragolha <luka.100.nerozi@gmail.com>
2023-08-23 13:33:17 -0300

    teste

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outlanders</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="navbar-container">
+        <div class="navbar-content">
+            <div class="logo">OUTLANDERS</div>
+
+            <input class="input-search" type="serach" placeholder="Search" />
+            <div class="navbar-itens">
+                <a class="link-styles" href="/">Services</a>
+                <a class="link-styles" href="/">About</a>
+                <a href="register.php"><input class="buttonRegister" type="button" value="Register" /></a>
+
+            </div>
+        </div>
+    </div>
+    <div class="hero-container">
+        <div class="hero-content">
+            <div class="hero-box">
+                <div class="title">Hero text, titulo de proposta.</div>
+                <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae pretium felis.
+                    Vivamus feugiat eros sagittis ornare accumsan.
+                </div>
+                <div class="hero-button"><input class="buttonRegister" href="login.php" type="button" value="Get this!" /></div>
+            </div>
+            <div class="image"><img class="imagefull" src="./img/8084190_1188.jpg" /></div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ target.md
@@ -0,0 +1,24 @@
+# Objetivos do projeto
+
+## Protótipo
+### Criar 3 telas
+- LP simples.
+- register.
+- login.
+- visualização de usúarios.
+---
+## Criação do banco de dados
+### Tabela de usuário.
+- id.
+- username.
+- email.
+- password.
+---
+
+# Dev Front
+- Realizar todas as paginas.
+
+
+
+
+
